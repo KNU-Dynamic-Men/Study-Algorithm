9a0ded15e30148c248819cb5d6ee0fbfce31dafa
Kim woo seob <rokuta1059@gmail.com>
2020-07-13 10:38:40 +0900

    10828 완료
    10828 스택 코드와 풀이

---
+++ baekjoon/10828-스택/우섭/10828.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+for _ in range(n):
+    input = sys.stdin.readline()
+    if 'push' in input:
+        stack.append(int(input.split()[1]))
+    elif 'pop' in input:
+        print(stack.pop() if len(stack) > 0 else -1)
+    elif 'size' in input:
+        print(len(stack))
+    elif 'empty' in input:
+        print(1 if len(stack) == 0 else 0)
+    elif 'top' in input:
+        print(stack[-1] if len(stack) != 0 else -1)
+
+print(lambda: stack[-1] if len(stack) != 0 else -1)
