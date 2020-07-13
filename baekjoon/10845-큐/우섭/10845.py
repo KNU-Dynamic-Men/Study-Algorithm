f255ac7cc63dd40de4b6d7d99d45839c1279f1bd
Kim woo seob <rokuta1059@gmail.com>
2020-07-13 10:57:55 +0900

    10845 완료
    10845 큐 코드와 풀이

---
+++ baekjoon/10845-큐/우섭/10845.py
@@ -0,0 +1,18 @@
+import sys
+
+n = int(sys.stdin.readline())
+stack = []
+for _ in range(n):
+    input = sys.stdin.readline()
+    if 'push' in input:
+        stack.append(int(input.split()[1]))
+    elif 'pop' in input:
+        print(stack.pop(0) if len(stack) > 0 else -1)
+    elif 'size' in input:
+        print(len(stack))
+    elif 'empty' in input:
+        print(1 if len(stack) == 0 else 0)
+    elif 'front' in input:
+        print(stack[0] if len(stack) != 0 else -1)
+    elif 'back' in input:
+        print(stack[-1] if len(stack) != 0 else -1)
