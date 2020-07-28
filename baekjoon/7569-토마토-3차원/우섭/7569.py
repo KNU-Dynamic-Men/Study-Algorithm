4b0d7aefd63213602116a9a6335140dce77304aa
Kim woo seob <rokuta1059@gmail.com>
2020-07-28 12:20:07 +0900

    7569 완료
    7569 토마토 코드와 풀이

---
+++ baekjoon/7569-토마토-3차원/우섭/7569.py
@@ -0,0 +1,47 @@
+from collections import deque
+from sys import stdin
+M, N, H = map(int, stdin.readline().split())
+box = []
+dx = [1, 0, -1, 0, 0, 0]
+dy = [0, 1, 0, -1, 0, 0]
+dz = [0, 0, 0, 0, 1, -1]
+
+tomato = [[[-1]*M for _ in range(N)] for _ in range(H)]
+search = deque()
+
+box = [[list(map(int, stdin.readline().rstrip().split())) for i in range(N)] for j in range(H)]
+
+for k in range(H):
+    for i in range(N):
+        for j in range(M):
+            if box[k][i][j] == 1:
+                search.append([k, i, j])
+                tomato[k][i][j] = 0
+            if box[k][i][j] == -1:
+                tomato[k][i][j] = 0
+while(search):
+    z, x, y = map(int, search.popleft())
+    for i in range(6):
+        nz = z + dz[i]
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or nx >= N or ny < 0 or ny >= M or nz < 0 or nz >= H:
+            continue
+        if tomato[nz][nx][ny] >= 0:
+            continue
+        tomato[nz][nx][ny] = tomato[z][x][y] + 1
+        search.append([nz, nx, ny])
+
+find = True
+maximum = 0
+for k in range(H):
+    for i in range(N):
+        for j in range(M):
+            if tomato[k][i][j] == -1:
+                print(-1)
+                find = False
+        if not find: break
+        maximum = max(maximum, max(tomato[k][i]))
+    if not find: break
+if find:
+    print(maximum)
